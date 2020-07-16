3f87a32c1b7f5dadbb3b82aeffdd6537b69568b5
Terry Chen <chenterry85@gmail.com>
2020-07-16 23:37:06 +0800

    Initial Commit

---
+++ Oni/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Oni
+//
+//  Created by Terry Chen on 2020/7/16.
+//  Copyright © 2020 Terry Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
