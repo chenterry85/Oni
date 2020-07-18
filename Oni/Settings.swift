2353fbd9bc13270f2c4e4d8ebc0431e73354e4c9
Terry Chen <chenterry85@gmail.com>
2020-07-18 22:11:36 +0800

    Add Settings class

---
+++ Oni/Settings.swift
@@ -0,0 +1,14 @@
+//
+//  Settings.swift
+//  Oni
+//
+//  Created by Terry Chen on 2020/7/18.
+//  Copyright © 2020 Terry Chen. All rights reserved.
+//
+
+import Foundation
+
+struct Settings{
+    static backgroundThemeColor = "Black"
+    
+}
