48c10702bdad6de0c1d7173e3bb163059956ac1c
Terry Chen <chenterry85@gmail.com>
2020-07-25 12:29:09 +0800

    Init User class

---
+++ Oni/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Oni
+//
+//  Created by Terry Chen on 2020/7/25.
+//  Copyright © 2020 Terry Chen. All rights reserved.
+//
+
+import Foundation
+
+class User{
+    
+    init(){
+        
+    }
+    
+}
