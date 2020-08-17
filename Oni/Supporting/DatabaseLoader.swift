3aa623469574513ded0e2f9bb00a942c19817ca2
Terry Chen <chenterry85@gmail.com>
2020-08-18 00:02:55 +0800

    Init Database Loader

---
+++ Oni/Supporting/DatabaseLoader.swift
@@ -0,0 +1,59 @@
+//
+//  DatabaseLoader.swift
+//  Oni
+//
+//  Created by Terry Chen on 2020/8/17.
+//  Copyright © 2020 Terry Chen. All rights reserved.
+//
+
+import Foundation
+import SQLite3
+
+class DatabaseLoader{
+    
+    var db: OpaquePointer!
+    let dbPath = Bundle.main.path(forResource: "US-all-data", ofType: "db") ?? ""
+    static let shared = DatabaseLoader()
+
+    private init() {}
+//
+//    func searchStocks(withInput: String) -> [DB_Stock] {
+//        let sqlCommand = "SELECT * FROM  'US-all-data' WHERE symbol  LIKE '\(withInput)%'"
+//        var statement: OpaquePointer!
+//        var searchResult = [DB_Stock]()
+//        
+//        connectDatabase()
+//        
+//        if sqlite3_prepare_v2(db, sqlCommand, -1, &statement, nil) == SQLITE_OK{
+//            print("db: prepare_v2 success")
+//        }else{
+//            print("db: prepare_v2 failed")
+//        }
+//        
+//        while sqlite3_step(statement) == SQLITE_ROW{
+//            var currency = "", description = "", displaySymbol = "", symbol = "", type = ""
+//
+//            if let col0 = sqlite3_column_text(statement, 0){ currency = String(cString: col0) }
+//            if let col1 = sqlite3_column_text(statement, 1){ description = String(cString: col1) }
+//            if let col2 = sqlite3_column_text(statement, 2){ displaySymbol = String(cString: col2) }
+//            if let col3 = sqlite3_column_text(statement, 3){ symbol = String(cString: col3) }
+//            if let col4 = sqlite3_column_text(statement, 4){ type = String(cString: col4) }
+//
+//            let extractedStock = DB_Stock(currency: currency, description: description, displaySymbol: displaySymbol, symbol: symbol, type: type)
+//            searchResult.append(extractedStock)
+//        }
+//        
+//        sqlite3_finalize(statement)
+//        return searchResult
+//    }
+
+    private func connectDatabase(){
+        if sqlite3_open(dbPath, &db) == SQLITE_OK{
+            print("db: open success")
+        }else{
+            print("db: open failed")
+        }
+    }
+    
+    
+}
